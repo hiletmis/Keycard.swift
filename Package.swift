50b51c1fc64265410ae2fdba7ee3a22a9456538c
Hayrettin İletmiş <31757771+hiletmis@users.noreply.github.com>
2022-09-14 10:53:53 +0300

    Update Package.swift
    sec conflict

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Keycard"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/status-im/secp256k1.swift.git", .branch("master")),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", .branch("master")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", .upToNextMinor(from: "2.4.3"))
     ],
