56c10755f3d10db077e434e8aae6857fba212f23
hiletmis <hayrettin@ennowallet.com>
2022-09-14 13:54:43 +0300

    conflict
    sec conflict 2

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Keycard",
-            dependencies: ["secp256k1", "CryptoSwift"]),
+            dependencies: ["CryptoSwift"]),
         .testTarget(
             name: "KeycardTests",
             dependencies: ["Keycard"]),
